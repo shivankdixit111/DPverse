b044eac4b164d20bae4bc361ea9ab10cccce6f1e
Shivank Dixit <shivankdixit@Shivanks-MacBook-Air.local>
2025-05-17 20:54:30 +0530

    updated

--- backend/firebase.js
+++ backend/firebase.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 let admin = require("firebase-admin");
 
-let serviceAccount = require(process.env.GOOGLE_CREDENTIALS); //parse the path to your servce account key file
+let serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS); //parse the path to your servce account key file
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
