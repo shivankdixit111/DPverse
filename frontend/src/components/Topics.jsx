68d9c5d1471fa9d753f69722a10e6bce63530da0
Shivank Dixit <shivankdixit@Shivanks-MacBook-Air.local>
2025-10-20 18:49:31 +0530

    pushed

--- frontend/src/components/Topics.jsx
+++ frontend/src/components/Topics.jsx
@@ -3,7 +3,7 @@ import Topic from './Topic';
 import { userDataContext } from '../store/UserContext';
 import Loader from '../common/Loader';
 
-const topics = ["DP on Index", "DP on Subsequences", "Bitmask DP", "Range DP", "Digit DP", "DP on Grids", "DP on Tree/Graph", "DP on Trie", "Backtrack & Print"];
+const topics = ["DP on Index", "DP on Subsequences", "Bitmask DP", "Range DP", "Digit DP", "DP on Grids", "Re-rooting Tree DP", "DP on Trie", "Backtrack & Print"];
 
 const Topics = () => {
   const {
